2cddd5716389a1adbf5bb83f6392e5551d3e58da
hwat212 <73551461+hwat212@users.noreply.github.com>
2024-04-20 21:47:19 -0400

    Comments in types.h

--- src/types.h
+++ src/types.h
@@ -1,6 +1,4 @@
-//
-// Created by rgarber11 on 4/4/24.
-//
+// Copyright (c) 2024 Compile Collective. All Rights Reserved.
 
 #ifndef SENIORPROJECT_TYPES_H
 #define SENIORPROJECT_TYPES_H
@@ -9,17 +7,22 @@
 #include <variant>
 #include <vector>
 struct Type;
+// Conversion possibilities
 enum class Convert { SAME, IMPLICIT, EXPLICIT, FALSE };
+// Primary types
 enum class BottomType { INT, CHAR, BOOL, FLOAT, VOID, SELF };
+// Optional type (may or may not have value)
 struct OptionalType {
   std::shared_ptr<Type> optional;
   OptionalType(std::shared_ptr<Type> optional_type);
   OptionalType(const OptionalType& optional_type);
   OptionalType(OptionalType&& optional_type) noexcept;
 };
+// Tuple type (collection of types)
 struct TupleType {
   std::vector<std::shared_ptr<Type>> types;
 };
+// List type - size and type
 struct ListType {
   int size;
   std::shared_ptr<Type> type;
@@ -27,9 +30,11 @@ struct ListType {
   ListType(const ListType& list_type);
   ListType(ListType&& list_type) noexcept;
 };
+// Sum type - list of types
 struct SumType {
   std::vector<std::shared_ptr<Type>> types;
 };
+// Function type - parameters and return type
 struct FunctionType {
   std::shared_ptr<Type> returner;
   std::vector<std::shared_ptr<Type>> parameters;
@@ -40,6 +45,7 @@ struct FunctionType {
   FunctionType(const FunctionType& function_type);
   FunctionType(FunctionType&& function_type) noexcept;
 };
+// Alias type - alias and type
 struct AliasType {
   std::string alias;
   std::shared_ptr<Type> type;
@@ -49,24 +55,30 @@ struct AliasType {
   AliasType(const AliasType& alias_type);
   AliasType(AliasType&& alias_type) noexcept;
 };
+// Struct type - vector of alias types
 struct StructType {
   std::vector<AliasType> types;
 };
+// Impl - list of alias types it includes
 struct Impl {
   std::vector<AliasType> includes;
 };
+// Every expression is one of the preceding types
 using InnerType =
     std::variant<BottomType, OptionalType, TupleType, ListType, StructType,
                  SumType, FunctionType, AliasType, Impl>;
+// Main type information
 struct Type {
   InnerType type;
   std::vector<std::shared_ptr<Impl>> interfaces;
   Type(const InnerType& type,
        const std::vector<std::shared_ptr<Impl>>& interfaces);
   Type(const Type& type_t);
+  // Clone a pointer to this type
   [[nodiscard]] std::shared_ptr<Type> clone() const {
     return std::make_shared<Type>(type, interfaces);
   }
+  // Check if type is of various types
   bool isBottomType() const {
     return std::visit(
         [](auto&& arg) {
@@ -130,6 +142,7 @@ struct Type {
         },
         type);
   }
+  // Return a type of various types
   BottomType getBottomType() {  // NOTE BECAUSE ENUM RETURNS VALUE
     return std::get<BottomType>(type);
   };
