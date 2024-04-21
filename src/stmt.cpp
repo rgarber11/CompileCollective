8850d0a2552e9bcae5dd8e605084a1fcffb8dfbd
hwat212 <73551461+hwat212@users.noreply.github.com>
2024-04-20 20:58:19 -0400

    Comments in stmt.cpp

--- src/stmt.cpp
+++ src/stmt.cpp
@@ -1,17 +1,18 @@
-//
-// Created by rgarber11 on 4/4/24.
-//
+// Copyright (c) 2024 Compile Collective. All Rights Reserved.
 
 #include "stmt.h"
+// Constructors and destructors
 Stmt::Stmt(const Stmt& stmt) : location(stmt.location), type(stmt.type), stmt(stmt.stmt) {}
 Stmt::Stmt(Stmt&& stmt) noexcept : location(stmt.location), type(std::move(stmt.type)), stmt(std::move(stmt.stmt)) {}
 Stmt::~Stmt() = default;
 ClassStmt::ClassStmt(const ClassStmt& class_stmt) : name(class_stmt.name), structType(class_stmt.structType){
+  // Add parameters
   for(auto& stmt : class_stmt.parameters) {
     this->parameters.emplace_back(stmt);
   }
 }
 ClassStmt::ClassStmt(ClassStmt&& class_stmt) noexcept : name(std::move(class_stmt.name)), structType(class_stmt.structType) {
+  // Add parameters
   for(auto stmt : class_stmt.parameters) {
     this->parameters.emplace_back(std::move(stmt));
   }
@@ -19,11 +20,13 @@ ClassStmt::ClassStmt(ClassStmt&& class_stmt) noexcept : name(std::move(class_stm
 
 ClassStmt::~ClassStmt() = default;
 ImplStmt::ImplStmt(const ImplStmt& impl_stmt) : name(impl_stmt.name), decorating(impl_stmt.decorating), implType(impl_stmt.implType) {
+  // Add parameters
   for(auto& stmt : impl_stmt.parameters) {
     this->parameters.emplace_back(stmt);
   }
 }
 ImplStmt::ImplStmt(ImplStmt&& impl_stmt) noexcept : name(std::move(impl_stmt.name)), decorating(std::move(impl_stmt.decorating)), implType(std::move(impl_stmt.implType)) {
+  // Add parameters
   for(auto stmt : impl_stmt.parameters) {
     this->parameters.emplace_back(std::move(stmt));
   }
@@ -31,6 +34,7 @@ ImplStmt::ImplStmt(ImplStmt&& impl_stmt) noexcept : name(std::move(impl_stmt.nam
 ImplStmt::~ImplStmt() = default;
 DeclarationStmt::DeclarationStmt(const DeclarationStmt& declaration_stmt) : consted(declaration_stmt.consted), name(declaration_stmt.name), val(declaration_stmt.val ? declaration_stmt.val->clone() : nullptr) {}
 DeclarationStmt::DeclarationStmt(DeclarationStmt&& declaration_stmt) noexcept : consted(declaration_stmt.consted), name(std::move(declaration_stmt.name)), val(std::move(declaration_stmt.val)) {}
+// Copy another declaration statement
 DeclarationStmt& DeclarationStmt::operator=(DeclarationStmt&& other) noexcept{
     consted = other.consted;
     name = other.name;
